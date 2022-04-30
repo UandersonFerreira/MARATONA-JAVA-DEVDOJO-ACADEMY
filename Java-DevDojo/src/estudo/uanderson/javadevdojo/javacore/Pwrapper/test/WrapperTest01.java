f028ad14b1261144ae9756ac26685ce0be0d51f4
UandersonFerreira <uanderson.oliveira@estudante.ifto.edu.br>
2022-04-30 19:34:49 -0300

    wrappers commit

---
+++ Java-DevDojo/src/estudo/uanderson/javadevdojo/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,30 @@
+package estudo.uanderson.javadevdojo.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP =1;
+        int intP = 1;
+        long longP =10L;
+        float floatP =10F;
+        double doubleP = 10.55;
+        char charP = 'U';
+        boolean booleanP = false;
+
+
+        byte byteW = 1;
+        short shortW =1;
+        int intW = 1;
+        long longW =10L;
+        float floatW =10F;
+        double doubleW = 10.55;
+        char charW = 'U';
+        boolean booleanW = false;
+
+
+    }//main
+}//class
+/*
+Wrappers são classe que basicamente, encapsula os tipos
+primitivos e transforma-los em um Objeto(class).
+ */
