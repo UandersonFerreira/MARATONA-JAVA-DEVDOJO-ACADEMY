379604ac78e2b519624eaeeabc182d13419fe396
Uanderson <uanderson.oliveira@estudante.ifto.edu.br>
2022-06-28 12:13:24 -0300

    Trabalhando com datas

--- Java-DevDojo/src/estudo/uanderson/javadevdojo/javacore/Sformatacao/test/NumberFormatTest01.java
+++ Java-DevDojo/src/estudo/uanderson/javadevdojo/javacore/Sformatacao/test/NumberFormatTest01.java
@@ -1,6 +1,7 @@
 package estudo.uanderson.javadevdojo.javacore.Sformatacao.test;
 
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Locale;
 
 public class NumberFormatTest01 {
@@ -19,9 +20,35 @@ public class NumberFormatTest01 {
          double valor = 100_000.25;
 
         for (NumberFormat numberFormat : nfa) {
+            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(valor));
         }
 
+        String valorString = "10000";
+
+        try {
+            System.out.println(nfa[1].parse(valorString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+/*
+Observações: Apesar da sintaxe: 10_000; se valida para os números,
+quando se está trabalhando com a converção de um string para numero(parse)
+a sintaxe: "10_000"; possui algumas particularidades, tais como: o underline(_) é considerado
+como um um character invalido(pois não é visto como um número ou parte dele), portando ao ser fazer
+o passe o java quando se depara com um character inválido, irá ignora
+tudo o que estiver a sua direita, por isso que a impressa saiu 10;
+
+        String valorString = "10_000";
+
+        try {
+            System.out.println(nfa[1].parse(valorString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        resultado : 10
+ */
 
     }//main
 }//class
